2c954a5ee7eb996ad71d3608bbe8a4eb5900158d
Julia Lepner <ylepner@gmail.com>
2023-07-09 22:06:28 +0200

    feat: create app state

---
+++ src/app.state.ts
@@ -0,0 +1,19 @@
+export interface AppState {
+  rooms: Room[]
+}
+export type UserId = number;
+
+interface Room {
+  player1: number;
+  player2?: number;
+  game?: Game;
+}
+
+interface Game {
+
+}
+
+interface User {
+  name: string;
+  id: number;
+}
