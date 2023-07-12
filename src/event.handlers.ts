22a8c96705e9d833024062abc05b6de35dda2079
Julia Lepner <ylepner@gmail.com>
2023-07-12 09:04:32 +0200

    fix: correct roomId

--- src/event.handlers.ts
+++ src/event.handlers.ts
@@ -1,6 +1,6 @@
-import { AppState } from "./app.state";
-import { FindByType } from "./common";
-import { AppEvent, AppEventType } from "./events";
+import { AppState } from './app.state';
+import { FindByType } from './common';
+import { AppEvent, AppEventType } from './events';
 
 type EventOf<T extends AppEventType> = FindByType<AppEvent, T>;
 
@@ -8,57 +8,75 @@ export class ValidationError {
   constructor(public readonly reason: string) { }
 }
 
-function createEventHandler<T extends AppEventType>(eventType: T, handler: (event: EventOf<T>, state: AppState) => AppState) {
+function createEventHandler<T extends AppEventType>(
+  eventType: T,
+  handler: (event: EventOf<T>, state: AppState) => AppState,
+) {
   return {
     eventType,
     handler,
-  }
+  };
 }
-
-const roomCreatedHandler = createEventHandler('room_created', (event, state) => {
-  return {
-    ...state,
-    rooms: [...state.rooms, {
-      player1: event.ownerId,
-      id: state.rooms.length,
-    }]
-  }
-})
-
-const userRegisteredHandler = createEventHandler('user_registered', (event, state) => {
-  return {
-    ...state,
-    users: [...state.users, {
-      id: event.id,
-      name: event.name
-    }]
-  }
-})
 let roomCounter = 0;
+const roomCreatedHandler = createEventHandler(
+  'room_created',
+  (event, state) => {
+    return {
+      ...state,
+      rooms: [
+        ...state.rooms,
+        {
+          player1: event.ownerId,
+          id: roomCounter++,
+        },
+      ],
+    };
+  },
+);
 
-const addUserToRoomHandler = createEventHandler('user_added_to_room', (event, state) => {
-  const rooms = [...state.rooms];
-  const room = rooms.find((room) => room.id === event.roomId);
-  if (!room) {
-    throw new ValidationError(`Room with id ${event.roomId} is not found`);
-  }
-  const roomId = roomCounter++;
-  return {
-    ...state,
-    games: {
-      ...state.games,
-      [roomId]: {
-        id: roomId,
-        ownerId: room.player1,
-        players: {
-          [room.player1]: {},
-          [event.userId]: {}
-        }
-      }
-    },
-    rooms: rooms.filter((el) => el !== room),
-  }
-})
+const userRegisteredHandler = createEventHandler(
+  'user_registered',
+  (event, state) => {
+    return {
+      ...state,
+      users: [
+        ...state.users,
+        {
+          id: event.id,
+          name: event.name,
+        },
+      ],
+    };
+  },
+);
+
+
+const addUserToRoomHandler = createEventHandler(
+  'user_added_to_room',
+  (event, state) => {
+    const rooms = [...state.rooms];
+    const room = rooms.find((room) => room.id === event.roomId);
+    if (!room) {
+      throw new ValidationError(`Room with id ${event.roomId} is not found`);
+    }
+    const roomId = room.id;
+    return {
+      ...state,
+      games: {
+        ...state.games,
+        [roomId]: {
+          id: roomId,
+          ownerId: room.player1,
+          players: {
+            [room.player1]: {},
+            [event.userId]: {},
+          },
+        },
+      },
+      rooms: rooms.filter((el) => el !== room),
+    };
+  },
+);
 
 const shipsAdded = createEventHandler('ships_added', (event, state) => {
   const ships = event.ships;
@@ -71,12 +89,17 @@ const shipsAdded = createEventHandler('ships_added', (event, state) => {
         players: {
           ...state.games[event.gameId],
           [event.userId]: {
-            ships: ships
-          }
-        }
-      }
-    }
-  }
-})
+            ships: ships,
+          },
+        },
+      },
+    },
+  };
+});
 
-export const eventHandlers = [roomCreatedHandler, userRegisteredHandler, addUserToRoomHandler, shipsAdded];
+export const eventHandlers = [
+  roomCreatedHandler,
+  userRegisteredHandler,
+  addUserToRoomHandler,
+  shipsAdded,
+];
